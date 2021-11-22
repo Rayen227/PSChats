49daa43ca110faca6802a1bf430f5a22ec25a438
user_DE <16607615570@163.com>
2021-11-23 00:57:19 +0800

    test db

---
+++ db/dbmanager.py
@@ -0,0 +1,18 @@
+import requests
+
+host = 'http://47.107.112.158:5000'
+
+class DBManager:
+    def __init__(self):
+        pass
+
+    def addUser(self,uid,uname,usex):
+        url = host+'/adduser'
+        data = {'uid':uid,'uname':uname,'usex':usex}
+        print(data)
+        res = requests.post(url=url,data=data)
+        return res
+
+    def selectAllUser(self):
+        url = host + '/select'
+        return requests.post(url=url)
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import sys
 from scripts import  index
+from db import dbmanager
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    MainWindow = QMainWindow()
-    ui= index.MainWindowController(MainWindow)
-    MainWindow.show()
+    # app = QApplication(sys.argv)
+    # MainWindow = QMainWindow()
+    # ui= index.MainWindowController(MainWindow)
+    # MainWindow.show()
+    # sys.exit(app.exec_())
 
-    sys.exit(app.exec_())
+    # 测试数据库链接
+    dbm = dbmanager.DBManager()
+    # res = dbm.addUser('2111220001','test01','男')
+    res = dbm.selectAllUser()
+    print(res.content.decode('utf-8'))
